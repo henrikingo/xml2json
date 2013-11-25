88b4d593eb207627c92e0e3d3bdf7b3c1bd4d5f7
Henrik Ingo <henrik.ingo@10gen.com>
2013-11-25 11:09:51 +0200

    Initial commit. xml2json works, json2xml doesn't.

--- README.md
+++ README.md
@@ -2,3 +2,151 @@ xml2json
 ========
 
 Translate any XML document to corresponding JSON or vice versa
+
+HISTORY
+-------
+
+Most of this code was written by Stefan Gössner and published at:
+http://goessner.net/download/prj/jsonxml/
+
+You can read a description of what the library originally did at:
+http://www.xml.com/pub/a/2006/05/31/converting-between-xml-and-json.html
+
+I did 2 major changes to the original code:
+
+1. XML attributes are translated as siblings, rather than children, to their
+   corresponding "parent" element in JSON. This produces more **deterministic**
+   translation.
+2. Use JSON list structure more frequently. This ensures **lossless** translation
+   in more cases than the original and also is capable of handling some "mixed"
+   cases that the original would just pass along as untranslated XML.
+   
+Details of these changes are explained below. For anything else, read the xml.com
+article linked above.
+
+USAGE
+-----
+
+For example usage, see xmljson_test.html.
+
+The code itself also works and has been used as a node.js module together
+with the libxml parser, but you need to change a few lines to properly integrate
+as a node.js module (e.g. module.exports...)
+
+
+DETAILS
+-------
+
+An explanation of changes wrt the original library by Stefan Grössner follows:
+
+<e>text</e>
+{
+  "e":"text"
+}
+
+vs
+
+<e name="value">text</e>
+{
+  "e":{
+    "@name":"value",
+    "#text":"text"
+  }
+}
+
+=> the path to "text" changes depending on whether the element has attributes 
+   present or not. Very bad if there could be optional attributes, or attributes
+   added over time.
+
+For more examples, see http://goessner.net/download/prj/jsonxml/xmljson_test.html
+
+Solution is to lift out attributes to be on the same level as the element itself:
+
+<e name="value">text</e>
+{
+  e@name:"value",
+  e:"text"
+}
+
+Note that for empty elements, we still explicitly print null for clarity:
+
+<e name="value" />
+{
+  e@name:"value",
+  e:null
+}
+
+The other fix is to use lists to more closely resemble original xml structure
+in case of multiple children with same node name.
+
+Original:
+
+<e> <a>text</a> <a>text</a> </e>
+{
+  "e":{"a":[
+      "text",
+      "text"
+    ]}
+}
+
+Me:
+
+
+<e> <a>text</a> <a>text</a> </e>
+{
+  e:[
+        {
+      a:"text"
+    },
+        {
+      a:"text"
+    }
+  ]
+}
+
+This approach allows to handle "mixed content" elements better, where original
+gives up:
+
+<a>x<c/>y</a>
+{
+  "a":"x<c/>y"
+}
+
+Me:
+
+<a>x<c/>y</a>
+{
+  a:[
+    "x",
+        {
+      c:null
+    },
+    "y"
+  ]
+}
+
+(Same fix also supports CDATA sections, should there be any.)
+
+
+Not yet supported in xml->json:
+
+All types are strings. Preferably types would be defined in xml schema, and 
+the xml parser would return node values in correct javascript type. If this is
+not the case, we can emulate something by converting strings to other types in 
+a separate function to be done later (ie xml schema hard coded into a js function)
+For database searches, having the correct types will help.
+
+
+Not yet supported in json->xml:
+
+I have not yet made the corresponding changes to json2xml.js module, so it's not
+actually possible to take the output of xml2json.js and translate it back to xml.
+
+Multi-dimensional arrays. What is the correct xml presentation for this?
+
+{ e : [["a", "b"], ["c", "d"]] }
+
+
+
+
+
---
+++ xml2json.js
@@ -0,0 +1,213 @@
+/*	This work is licensed under Creative Commons GNU LGPL License.
+
+	License: http://creativecommons.org/licenses/LGPL/2.1/
+   Version: 0.10
+	Author:  Stefan Goessner/2006, Henrik Ingo/2013
+	Web:     https://github.com/henrikingo/xml2json 
+*/
+function xml2json(xml, tab) {
+   var X = {
+      err: function(msg) {
+         alert("Error: " + msg);
+      },
+      /**
+       * Return a js object from given xml. 
+       * We represent element attributes as siblings, not children, of their
+       * element, hence they need to be added to parent element.
+       */
+      toObj: function(xml, parent) {
+         if (xml.nodeType==9) // document.node
+            return X.toObj(xml.documentElement, parent);
+         
+         var o = {};
+
+         if(   !parent                    // no parent = root element = first step in recursion
+            || parent instanceof Array ){ // if parent is an Array, we cannot add attributes to it, so handle it with similar extra step as a root element
+            if (xml.nodeType==1) { // element node
+               o[xml.nodeName] = X.toObj(xml, o);
+            }
+            else
+               X.err("unhandled node type: " + xml.nodeType);
+            return o;
+         }
+
+         // second and following recursions      
+         if (xml.nodeType==1) {   // element node ..
+
+            if (xml.attributes.length)   // element with attributes  ..
+               for (var i=0; i<xml.attributes.length; i++)
+                  parent[xml.nodeName+"@"+xml.attributes[i].nodeName] = xml.attributes[i].nodeValue;
+
+            if (xml.firstChild) { // element has child nodes. Figure out some properties of it's structure, to guide us later.
+               var textChild=0, cdataChild=0, hasElementChild=false, needsArray=false;
+               var elemCount = {};
+               for (var n=xml.firstChild; n; n=n.nextSibling) {
+                  if (n.nodeType==1) {
+                     hasElementChild = true;
+                     elemCount[n.nodeName] = (elemCount[n.nodeName] ? elemCount[n.nodeName] + 1 : 1);
+                     if( elemCount[n.nodeName] > 1 ) needsArray=true;
+                  }
+                  else if (n.nodeType==3 && n.nodeValue.match(/[^ \f\n\r\t\v]/)) textChild++; // non-whitespace text
+                  else if (n.nodeType==4) cdataChild++; // cdata section node
+               }
+               if (hasElementChild && textChild) needsArray=true;
+               if (hasElementChild && cdataChild) needsArray=true;
+               if (textChild && cdataChild) needsArray=true;
+               if (cdataChild > 1) needsArray=true;
+               
+               if (hasElementChild && !needsArray) { // Neatly structured and unique child elements, no plain text/cdata in the mix
+                     X.removeWhite(xml);
+                     for (var n=xml.firstChild; n; n=n.nextSibling) {
+                        if (n.nodeType == 3)  // text node
+                           //o["#text"] = X.escape(n.nodeValue);
+                           o["#text"] = X.escape(n.nodeValue);
+                        else if (n.nodeType == 4)  // cdata node
+                           o["#cdata"] = X.escape(n.nodeValue);
+                        else if (o[n.nodeName]) {  // multiple occurence of element ..
+                           if (o[n.nodeName] instanceof Array)
+                              o[n.nodeName][o[n.nodeName].length] = X.toObj(n, o);
+                           else
+                              o[n.nodeName] = [o[n.nodeName], X.toObj(n, o)];
+                        }
+                        else  // first occurence of element..
+                           o[n.nodeName] = X.toObj(n, o);
+                     }
+               }
+               else if ( needsArray ) {
+                     o = [];
+                     X.removeWhite(xml);
+                     for (var n=xml.firstChild; n; n=n.nextSibling) {
+                        if (n.nodeType == 3)  // text node
+                           //o["#text"] = X.escape(n.nodeValue);
+                           o[o.length] = X.escape(n.nodeValue); // TODO: shouldn't escape() happen in toJson() / printing phase???
+                        else if (n.nodeType == 4)  // cdata node
+                           o[o.length] = { "#cdata" : X.escape(n.nodeValue) }; // TODO: same here? especially with cdata?
+                        else  { // element
+/*                           // at least in browser, cannot create new object with value of a variable as key
+                           var newObj = {}; 
+                           // must set the key here separately
+                           newObj[n.nodeName] = X.toObj(n, o); 
+                           o[o.length] = newObj; // push
+*/
+                           o[o.length] = X.toObj(n, o); //push
+                        }
+                     }
+               }
+               else if (textChild) { // pure text
+                  o = X.escape(X.innerXml(xml));
+               }
+               else if (cdataChild) { // single cdata
+                  X.removeWhite(xml);
+                  o["#cdata"] = X.escape(xml.firstChild.nodeValue);
+               }
+            }
+
+            //if (!xml.attributes.length && !xml.firstChild) o = null;
+            if (!xml.firstChild) o = null;
+
+         }
+         else
+            X.err("unhandled node type: " + xml.nodeType);
+
+         return o;
+      },
+      toJson: function(o, ind) {
+         var json = "";
+         if (o instanceof Array) {
+            for (var i=0,n=o.length; i<n; i++) {
+               // strings usually follow the colon, but in arrays we must add the usual indent
+               var extra_indent = "";
+               if ( typeof(o[i]) == "string" )
+                   extra_indent = ind+"\t";
+               o[i] = extra_indent + X.toJson(o[i], ind+"\t");
+            }
+            json += "[" + (o.length > 1 ? ("\n"+o.join(",\n")+"\n"+ind) : o.join("")) + "]";
+         }
+         else if (o == null)
+            json += "null";
+         else if (typeof(o) == "string")
+            json += "\"" + o.toString() + "\"";
+         else if (typeof(o) == "object") {
+            json += ind+"{";
+            // Count the members in o
+            var i = 0;
+            for (var member in o) 
+                i++;
+            // ...so that we know when we are at the last element when doing this
+            for (var member in o) {
+                json += "\n"+ ind + "\t" + member + ":" + X.toJson(o[member], ind+"\t");
+                json += (i > 1 ? "," : "\n"+ind ); 
+                i--;
+            }
+            json += "}";            
+         }
+         else
+            json += o.toString();
+         return json;
+      },
+      innerXml: function(node) {
+         var s = ""
+         if ("innerHTML" in node)
+            s = node.innerHTML;
+         else {
+            var asXml = function(n) {
+               var s = "";
+               if (n.nodeType == 1) {
+                  s += "<" + n.nodeName;
+                  for (var i=0; i<n.attributes.length;i++)
+                     s += " " + n.attributes[i].nodeName + "=\"" + (n.attributes[i].nodeValue||"").toString() + "\"";
+                  if (n.firstChild) {
+                     s += ">";
+                     for (var c=n.firstChild; c; c=c.nextSibling)
+                        s += asXml(c);
+                     s += "</"+n.nodeName+">";
+                  }
+                  else
+                     s += "/>";
+               }
+               else if (n.nodeType == 3)
+                  s += n.nodeValue;
+               else if (n.nodeType == 4)
+                  s += "<![CDATA[" + n.nodeValue + "]]>";
+               return s;
+            };
+            for (var c=node.firstChild; c; c=c.nextSibling)
+               s += asXml(c);
+         }
+         return s;
+      },
+      escape: function(txt) {
+         return txt.replace(/[\\]/g, "\\\\")
+                   .replace(/[\"]/g, '\\"')
+                   .replace(/[\n]/g, '\\n')
+                   .replace(/[\r]/g, '\\r');
+      },
+      removeWhite: function(e) {
+         e.normalize();
+         for (var n = e.firstChild; n; ) {
+            if (n.nodeType == 3) {  // text node
+               if (!n.nodeValue.match(/[^ \f\n\r\t\v]/)) { // pure whitespace text node
+                  var nxt = n.nextSibling;
+                  e.removeChild(n);
+                  n = nxt;
+               }
+               else
+                  n = n.nextSibling;
+            }
+            else if (n.nodeType == 1) {  // element node
+               X.removeWhite(n);
+               n = n.nextSibling;
+            }
+            else                      // any other node
+               n = n.nextSibling;
+         }
+         return e;
+      }
+   };
+   if (xml.nodeType == 9) // document node
+      xml = xml.documentElement;
+   var o = X.toObj(X.removeWhite(xml));
+   var json = X.toJson(o, "");
+   // If tab given, do pretty print, otherwise remove white space
+   return (tab ? json.replace(/\t/g, tab) : json.replace(/\t|\n/g, ""));
+}
